227bdf5bfa5f342fc090abbbfe1ed1735ead2917
Chloe Skorpikova <cskorpi@wgu.edu>
2022-07-12 15:57:34 -0700

    Couldn't get Heroku to work... Changed back to dev, so I can work with building it out.

--- run.py
+++ run.py
@@ -2,14 +2,12 @@ from app import create_app, db
 from app.auth.models import User
 from sqlalchemy import exc
 
-flask_app = create_app('prod')
+flask_app = create_app('dev')
 with flask_app.app_context():
     db.create_all()
-
-    try:
+    if __name__ == "__main__":
         if not User.query.filter_by(user_name='harry').first():
             User.create_user(user='harry',
                              email='harry@potters.com',
                              password='secret')
-    except exc.IntegrityError:
         flask_app.run()
