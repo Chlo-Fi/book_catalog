182deee5c209c40eead210396d71c6529011b13f
Chloe Skorpikova <cskorpi@wgu.edu>
2022-07-12 15:26:45 -0700

    Removed "if name == main" block and updated database url

--- app/config/prod.py
+++ app/config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = False
 SECRET_KEY='topsecret'
-SQLALCHEMY_DATABASE_URI='postgresql://chloe:TopSecret!42@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI= os.eviron['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
